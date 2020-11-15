3ff51ee8569b384438bb5a99bc78407d517d0bdd
Ofer Halevi <oferhalevi@gmail.com>
2020-11-15 13:12:29 +0800

    typo in formSubmit

--- functions/formSubmit.js
+++ functions/formSubmit.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    console.log(JSON.stinfigy(context));
+    console.log(JSON.stringify(context));
     return {
         statusCode: 200,
         body: JSON.stringify({})
