78f7d7164d776c8c5383abaad8ccd5df0b445166
Ofer Halevi <oferhalevi@gmail.com>
2020-11-15 13:20:27 +0800

    console log of event from handler

--- functions/formSubmit.js
+++ functions/formSubmit.js
@@ -1,5 +1,6 @@
 exports.handler = async function (event, context) {
     console.log(JSON.stringify(context));
+    console.log(JSON.stringify(event));
     return {
         statusCode: 200,
         body: JSON.stringify({})
